a0912e5c7a24943d9db90ffa9d863ddd35644f36
ba-09 <barbara.souza.coelho@escola.pr.gov.br>
2026-06-11 11:16:09 +0000

    aula 5

--- main.js
+++ main.js
@@ -15,3 +15,25 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 contadores[0].textContent = "Contagem regressiva";
+
+function calculaTempo(tempoObjetivo) {
+  let tempoAtual = new Date();
+  let tempoFinal = tempoObjetivo - tempoAtual;
+  let segundos = Math.floor(tempoFinal / 1000);
+  let minutos = Math.floor(segundos / 60);
+  let horas = Math.floor(minutos / 60);
+  let dias = Math.floor(horas / 24);
+  return (
+    dias +
+    " dias " +
+    horas +
+    " horas " +
+    minutos +
+    " minutos " +
+    segundos +
+    " segundos"
+  );
+}
+segundos %= 60;
+minutos %= 60;
+horas %= 24;
